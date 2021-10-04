465b5af6d551e976bf7edc585bb54c22dd099431
avm <avmiki80@mail.ru>
2021-10-04 23:55:01 +0500

    подготовка к выгрузке на хостинг

--- src/main/java/ru/avid/test/auth/utils/CookieUtils.java
+++ src/main/java/ru/avid/test/auth/utils/CookieUtils.java
@@ -37,7 +37,7 @@ public class CookieUtils {
     @Value("${cookie.jwt.max-age}")
     private int cookieAccessTokenDuration;
 
-    @Value("${server.domain}")
+    @Value("${cookie.domain}")
     private String cookieAccessTokenDomain;
 
 
